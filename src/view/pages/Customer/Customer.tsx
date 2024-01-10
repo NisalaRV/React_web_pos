2bf5881962cfeb2deb315c1472c275b2d972bf72
Nisala Ravihara <116428103+NisalaRV@users.noreply.github.com>
2024-01-10 23:57:22 +0530

    added customer table

--- src/view/pages/Customer/Customer.tsx
+++ src/view/pages/Customer/Customer.tsx
@@ -1,17 +1,55 @@
 import React from "react";
 import {
     Box,
-    Grid,
+    Grid, Paper, styled, Table, TableBody, TableCell, tableCellClasses, TableContainer, TableHead, TableRow,
     TextField,
 } from "@mui/material";
 import Header from "../../common/Header/Header";
 import Button from "@mui/material/Button";
+
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+        backgroundColor: theme.palette.common.black,
+        color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+        fontSize: 14,
+    },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+        border: 0,
+    },
+}));
+
+function createData(
+    Customer_Id: String,
+    CustomerName: String,
+    CustomerAddress: String,
+    Phone: String,
+) {
+    return {Customer_Id, CustomerName, CustomerAddress, Phone };
+}
+
+const rows = [
+    createData( 'C001', 'Nisala', 'Galle', '0776257843'),
+    createData( 'C002', 'Kavishka', 'Galle','0778957843' ),
+    createData( 'C003', 'Dineth', 'Galle', '0774857843'),
+    createData( 'C004', 'Ashan', 'Galle', '0776215443'),
+    createData( 'C005', 'Thishara', 'Galle', '0776589843'),
+];
 export default function Customer() {
     return(
         <>
         < Header/>
             <h1>Customer</h1>
-            <Grid lg={5} md={5} sm={10} xs={11} py={5} ml={5}>
+            <Grid lg={5}  md={5} sm={10} xs={11} ml={5} >
                 <Box
                     sx={{
                         bgcolor: "background.paper",
@@ -156,7 +194,42 @@ export default function Customer() {
                     </Box>
                 </Box>
             </Box>
-    </Grid>
+
+                <Grid container width={900} justifyContent={"center"}>
+                    <TableContainer component={Paper}>
+                        <Table sx={{ minWidth: 700}}  aria-label="customized table">
+                            <TableHead>
+                                <TableRow>
+                                    <StyledTableCell>Customer_Id</StyledTableCell>
+                                    <StyledTableCell align="right">Customer Name</StyledTableCell>
+                                    <StyledTableCell align="right">Customer Adress</StyledTableCell>
+                                    <StyledTableCell align="right">Phone</StyledTableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {rows.map((row) => (
+                                    <StyledTableRow>
+                                        <StyledTableCell component="th" scope="row">
+                                            {row.Customer_Id}
+                                        </StyledTableCell>
+                                        <StyledTableCell align="right">{row.CustomerName}</StyledTableCell>
+                                        <StyledTableCell align="right">{row.CustomerAddress}</StyledTableCell>
+                                        <StyledTableCell align="right">{row.Phone}</StyledTableCell>
+
+                                    </StyledTableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Grid>
+            </Grid>
     </>
     )
 };
+
+
+
+
+
+
+
